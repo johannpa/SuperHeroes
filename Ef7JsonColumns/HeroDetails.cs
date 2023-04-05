e7d7bd1ef8cf3cdec3a007b5998e1f985b7ff826
johannpa <johann_paque@hotmail.fr>
2023-04-05 06:33:15 +0200

    Super heroes with database

---
+++ Ef7JsonColumns/HeroDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace Ef7JsonColumns
+{
+    public class HeroDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? City { get; set; } = string.Empty;
+    }
+}
